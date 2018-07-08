98586c1b9f7155172d35e03f29f477ab39268e6f
willlawler <will.d.lawler@gmail.com>
2018-07-08 22:06:01 +1000

    Added location tracking and started on features

--- app/src/main/java/com/example/willlawler/trailapp/MainActivity.java
+++ app/src/main/java/com/example/willlawler/trailapp/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.willlawler.trailapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.widget.Toast;
 
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
 import com.esri.arcgisruntime.layers.ArcGISTiledLayer;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.portal.Portal;
 import com.esri.arcgisruntime.portal.PortalItem;
@@ -14,6 +25,7 @@ import com.esri.arcgisruntime.portal.PortalItem;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
+    private LocationDisplay mLocationDisplay;
 
 
     @Override
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         /*
         mMapView = findViewById(R.id.mapView);
         ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
@@ -31,10 +45,11 @@ public class MainActivity extends AppCompatActivity {
         Basemap basemap = new Basemap(tiledLayer);
 
 
-
         ArcGISMap map = new ArcGISMap();
         map.setBasemap(basemap);
         mMapView.setMap(map);
+        addTrailheadsLayer();
+        setupLocationDisplay();
     }
 
     @Override
@@ -54,4 +69,49 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         mMapView.dispose();
     }
+
+
+    private void addTrailheadsLayer() {
+        String url = "https://services9.arcgis.com/v4xqTIyE0gUuTGM9/arcgis/rest/services/testfeaturelayer/FeatureServer";
+        ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable(url);
+        FeatureLayer featureLayer = new FeatureLayer(serviceFeatureTable);
+        ArcGISMap map = mMapView.getMap();
+        map.getOperationalLayers().add(featureLayer);
+    }
+
+    private void setupLocationDisplay() {
+        mLocationDisplay = mMapView.getLocationDisplay();
+        mLocationDisplay.addDataSourceStatusChangedListener(new LocationDisplay.DataSourceStatusChangedListener() {
+            @Override
+            public void onStatusChanged(LocationDisplay.DataSourceStatusChangedEvent dataSourceStatusChangedEvent) {
+
+                if (dataSourceStatusChangedEvent.isStarted() || dataSourceStatusChangedEvent.getError() == null) {
+                    return;
+                }
+
+                int requestPermissionsCode = 2;
+                String[] requestPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+                if (!(ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[0]) == PackageManager.PERMISSION_GRANTED
+                        && ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[1]) == PackageManager.PERMISSION_GRANTED)) {
+                    ActivityCompat.requestPermissions(MainActivity.this, requestPermissions, requestPermissionsCode);
+                } else {
+                    String message = String.format("Error in DataSourceStatusChangedListener: %s",
+                            dataSourceStatusChangedEvent.getSource().getLocationDataSource().getError().getMessage());
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+        mLocationDisplay.startAsync();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mLocationDisplay.startAsync();
+        } else {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
